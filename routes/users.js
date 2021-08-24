9e4348f548596bb6dfae694591aee920467987c0
saiyidatul123 <saiyidatul123@gmail.com>
2021-08-24 15:00:24 +0800

    fixed server

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../model/helper");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  db("SELECT * FROM users;")
+  .then(results => {
+    res.send(results.data);
+  })
+  .catch(err => res.status(500).send(err));
 });
+
+const getAllUsers = (req, res) => {
+  db("SELECT * FROM users;")
+  .then(results => {
+    res.send(results.data);
+  })
+  .catch(err => res.status(500).send(err));
+};
+
+// GET one user
+router.get("/:id")
 
 module.exports = router;
