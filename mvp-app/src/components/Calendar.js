d6943f3c2b19b0d4a18a355de7d211f5b049f2ca
saiyidatul123 <saiyidatul123@gmail.com>
2021-08-27 07:29:13 +0800

    + Calendar package

--- mvp-app/src/components/Calendar.js
+++ mvp-app/src/components/Calendar.js
@@ -2,20 +2,20 @@ import React from 'react'
 
 const Calendar = () => {
 
-    const componentDidMount = () => {
-        const { match: { params } } = this.props;
+    // componentDidMount () {
+    //     const { match: { params } } = this.props;
       
-        fetch(`/tasks/user/${params.user_id}`)
-          .then(({ data: user }) => {
-            console.log('user', user);
+    //     fetch(`/tasks/user/${params.user_id}`)
+    //       .then(({ data: user }) => {
+    //         console.log('user', user);
       
-            this.setState({ user });
-          });
-      }
+    //         this.setState({ user });
+    //       });
+    //   }
     
     return (
         <div>
-            <h1>Ahoy there!</h1>
+            <h1>Calendar here</h1>
             
         </div>
     )
